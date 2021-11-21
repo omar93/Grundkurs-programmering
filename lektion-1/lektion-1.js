d8366cf987a3712b49d6b9b1a496d43819bdf33b
Omar Alhamad <omar93@hotmail.se>
2021-11-21 20:58:57 +0100

    adding lesson 1 files

---
+++ lektion-1/lektion-1.js
@@ -0,0 +1,13 @@
+/**
+ * Nedan har jag initierat 2 variabler till er, jag vill att ni deklarerar
+ * "playername" med en valfri sträng, gärna ett namn
+ * sedan deklarerar ni "hp" med ett nummer.
+ * 
+ * När ni är klara så kör ni kommandot "npm start"
+ * & ni ska få utskriften "Hello 'valfri sträng' you hp is 'valfritt nummer' "
+ */
+
+let playerName
+let hp
+
+console.log(`Hello ${playerName} you hp is ${hp}`)
